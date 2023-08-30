fa4aa7b90a265e922bf5020b984b34a94e15bd50
Mohsen Ali <mohsen.ali@xloopdigital.com>
2023-08-30 15:33:47 +0500

    React Pointer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [position, setPosition] = useState({x:0, y:0})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+     onPointerMove={e => {
+      setPosition({x:e.clientX, y:e.clientY})
+     }}
+     style={{
+      position: 'relative',
+      width: '100vw',
+      height: '100vh',
+    }}>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'red',
+        borderRadius: '50%',
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        left: -10,
+        top: -10,
+        width: 20,
+        height: 20,
+      }} />
+  
     </div>
   );
 }
